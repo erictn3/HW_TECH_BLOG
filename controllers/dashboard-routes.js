cd88617f85d9a934ac8ea6bc0070f424b15f3084
Eric Nguyen <erictn3@uci.edu>
2021-04-25 12:48:53 -0700

    routing

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,21 +1,52 @@
 const router = require('express').Router();
-// const withAuth = require('../utils/auth');
-// const { Post } = require('./home-routes');
+const withAuth = require('../utils/auth');
+const { Post } = require('./home-routes');
 
 // use withAuth middleware to prevent access to route
 // add with auth and async
 router.get('/', (req, res) => {
-  // try{
-  //   const postData = await Post.findAll({
-  //     where: {
-  //       userId: req.session.userId,
-  //     },
-  //   });
+  try {
+    const postData = await Post.findAll({
+      where: {
+        userId: req.session.userId,
+      },
+    });
 
-  //   const posts = postData.map((post) => post.get({ plain: true}));
+    const posts = postData.map((post) => post.get({ plain: true }));
 
-    res.render('login');
-  
-})
+    res.render('all-posts-admin', {
+      layout: 'dashboard',
+      posts,
+    });
+  } catch (err) {
+    res.redirect('login');
+  }
+});
+
+router.get('/new', withAuth, (req, res) => {
+  res.render('new-post', {
+    layout: 'dashboard',
+  });
+});
+
+router.get('/edit/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id);
+
+    if (postData) {
+      const post = postData.get({ plain: true });
+
+      res.render('edit-post', {
+        layout: 'dashboard',
+        post,
+      });
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.redirect('login');
+  }
+});
 
 module.exports = router;
+
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,21 +1,62 @@
 const router = require('express').Router();
-// const { Post } = require('../models/');
+const { Post, Comment, User } = require('../models/');
 
-router.get('/', (req, res) => {
-  
-  const body = {
-    title: "title",
-    name: "Eric"
+// get all posts for homepage
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [User],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render('all-posts', { posts });
+  } catch (err) {
+    res.status(500).json(err);
   }
-  
-  res.render('all-posts', {body});
-})
+});
+
+// get single post
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        User,
+        {
+          model: Comment,
+          include: [User],
+        },
+      ],
+    });
+
+    if (postData) {
+      const post = postData.get({ plain: true });
+
+      res.render('single-post', { post });
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
   res.render('login');
-})
+});
 
 router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
   res.render('signup');
-})
+});
+
 module.exports = router;
